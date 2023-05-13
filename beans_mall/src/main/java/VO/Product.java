d3e0f4239bf41a3a70ce4a2fd9060241a9acfab5
kyeon <kyeon@kkb>
2023-05-14 07:13:23 +0900

    쇼핑몰 홈페이지 뼈대 제작

---
+++ beans_mall/src/main/java/VO/Product.java
@@ -0,0 +1,17 @@
+package VO;
+
+public class Product {
+    private String name;
+    private String description;
+    private int price;
+
+    public Product(String name, String description, int price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    // Getter and Setter methods
+
+    // 생략
+}
