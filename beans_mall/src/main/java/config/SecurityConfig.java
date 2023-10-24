0d140f7669bda81c868ae54f0351192a5bba6aba
kyeon <kyeongbin3431@gmail.com>
2023-10-24 19:15:13 +0900

    김경빈의 프로젝트 메인화면 수정 및 보안관련 업데이트

--- beans_mall/src/main/java/config/SecurityConfig.java
+++ beans_mall/src/main/java/config/SecurityConfig.java
@@ -67,3 +67,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		return CookieCsrfTokenRepository.withHttpOnlyFalse();
 	}
 }
+
+
+//사용자 암호화: BCryptPasswordEncoder를 사용하여 사용자 비밀번호를 암호화하고 저장합니다.
+//세션 관리: 최대 동시 세션 수 설정 및 세션 무효화 설정을 통한 동시 로그인 제어.
+//CSRF 보호: CSRF 토큰을 사용하여 CSRF 공격을 방지합니다.
+//HTTP 헤더 보안: Content Security Policy (CSP) 헤더 및 HTTPS 강제 사용 설정.
+//권한 및 인가: 사용자 역할에 따라 접근 권한을 설정하고 권한 없는 경우 403 에러 페이지로 이동.
+//로그인 및 로그아웃 커스터마이징: 로그인 및 로그아웃 페이지 설정 및 세션 무효화.
+//사용자 정의 에러 페이지: 권한이 없는 경우 403 에러 페이지 설정.
